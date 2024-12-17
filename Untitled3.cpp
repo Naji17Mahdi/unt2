f3a0c7c849e250011783bc975d3c685261f54630
Mahdi Naji <mahdinaji754@gmail.com>
2024-12-17 10:49:06 +0330

    Create Untitled3.cpp

---
+++ Untitled3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+
+main()
+{
+	int a = 10;
+	int b = 98;
+	int i;
+	
+	for ( i = a ; i <= b ; i += 2 )
+	{
+		cout<<i<<endl;
+	}
+}
